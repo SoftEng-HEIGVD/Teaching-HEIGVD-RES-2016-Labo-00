55973c93c0ab56dd8b3f6488326e5c7391a24140
Laureline David <ltouroumov@gmail.com>
2016-02-24 14:22:27 +0100

    Fixed Addition bug in Applcation.java Created Instruments

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,15 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Project: Lab00App-build
+ * Created by jdavid on 24.02.16.
+ */
+public interface IInstrument {
+
+   String play();
+
+   int getSoundVolume();
+
+   String getColor();
+
+}
