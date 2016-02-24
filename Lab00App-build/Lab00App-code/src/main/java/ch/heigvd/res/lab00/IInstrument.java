faa94780c57764d23629a5e671ea6fd2e874bb6c
Madolyne Dupraz <madolyne.dupraz@heig-vd.ch>
2016-02-24 17:09:40 +0100

    Labo 00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by Mado on 24.02.2016.
+ */
+public interface IInstrument {
+
+    String play();
+    int getSoundVolume();
+    String getColor();
+}
