4d466e5c53b637658b31eef106982f112efa106d
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-02-24 17:05:21 +0100

    fixed bugs found by tests in TrumpetTest.java

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by Benjamin Schubert on 2/24/16.
+ */
+
+public interface IInstrument {
+    String play();
+    int getSoundVolume();
+    String getColor();
+}
