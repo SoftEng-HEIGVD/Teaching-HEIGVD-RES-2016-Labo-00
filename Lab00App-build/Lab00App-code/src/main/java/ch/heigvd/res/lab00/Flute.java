6ad6bbc92f6bd848de669ed5ad7aeff568122abe
Ioannis Noukakis <ioannis.noukakis@heig-vd.ch>
2016-02-25 13:08:51 +0100

    Implementation of instruments according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,32 @@
+/*
+ * HEIG-VD / POO 
+ * Ioannis Noukakis && Djomo Patrick Deslé
+ * Laboratoire N°09
+ * File : Flute.java
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author durza9390
+ */
+public class Flute implements IInstrument{
+    
+    @Override
+    public String play()
+    {
+        return "fuuuuiiii";
+    }
+    
+    @Override
+    public int getSoundVolume()
+    {
+        return 10;
+    }    
+    
+    @Override
+    public String getColor()
+    {
+        return "grey";
+    }
+}
