be1b7da5b0973fe102a4a47891c4aa3f3c6280e4
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-02-26 14:06:30 +0100

    Séparation des classes dans leurs fichier respectifs

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -5,31 +5,3 @@ public interface IInstrument {
 	int getSoundVolume();
 	String getColor();
 }
-
-class Trumpet implements IInstrument {
-	public String play() {
-		return "pouet";
-	}
-
-	public int getSoundVolume() {
-		return 10;
-	}
-
-	public String getColor() {
-		return "golden";
-	}
-}
-
-class Flute implements IInstrument {
-	public String play() {
-		return "fuu";
-	}
-
-	public int getSoundVolume() {
-		return 5;
-	}
-
-	public String getColor() {
-		return "white";
-	}
-}
