94176be9261357374e1047f27130692d65818f8e
Andrea Cotza <andrea.cotza@heig-vd.ch>
2016-02-26 12:44:44 +0000

    Correction of Application.java and implementation of instruments classes according to the JUnit test

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,17 @@
+/**
+ -----------------------------------------------------------------------------------
+ Laboratoire : 00
+ Fichier     : Flute.java
+ Auteur(s)   : Andrea Cotza
+ Date        : 24.02.2016
+ -----------------------------------------------------------------------------------
+*/
+
+package ch.heigvd.res.lab00;
+
+public class Flute extends Trumpet {
+
+   public int getSoundVolume() {
+      return 10;
+   }
+}
