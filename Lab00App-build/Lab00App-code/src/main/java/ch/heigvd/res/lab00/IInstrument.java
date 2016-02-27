db12a3cc9787c93be10012708e876a9e979c4484
Mathieu Urstein <mathieu.urstein@gmail.com>
2016-02-27 17:29:41 +0100

    Task 6 : Submit your results (with IntelliJ .iml files)

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,14 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by mathieuurstein on 27.02.16.
+ */
+public interface IInstrument {
+
+    String play();
+
+    int getSoundVolume();
+
+    String getColor();
+
+}
