5d605a14959fc0915d2a1dad6c08bb17527500a2
*Kevin <kevin.ponce@heig-vd.ch>
2016-02-28 22:40:45 +0100

    Labo00 Trumpet Flute IInstrument

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Trumpet implements IInstrument
+{
+   public String getColor(){
+       return "golden";
+   }
+    
+   
+   public String play(){
+       return "pouet";
+   }
+   
+   public int getSoundVolume(){
+       return 2;
+   }
+    
+}
