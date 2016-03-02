3ea25ae1b40ac447303e0b5dc1dd14317d20cd7f
Yves Athanasiadès <yves.athanasiades@heig-vd.ch>
2016-03-02 13:41:14 +0100

    Created Orchestra class

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -14,7 +14,7 @@ package ch.heigvd.res.lab00;
  --------------------------------------------------------------------------------*/
 public class Flute implements IInstrument {
    public String play() {
-      return "tuut";
+      return "trilili";
    }
 
    public int getSoundVolume() {
