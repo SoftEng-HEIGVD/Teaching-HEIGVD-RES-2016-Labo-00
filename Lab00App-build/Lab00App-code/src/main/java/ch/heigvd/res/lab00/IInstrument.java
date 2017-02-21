c8e982c2800325518df3aec2c4a5bab3fa959f91
David Truan <daxid@windowslive.com>
2017-02-21 14:48:34 +0100

    Implementation of Trumpet, IInstrument, Flute and Application

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author David Truan
+ */
+public interface IInstrument {
+    public String play();
+    public int getSoundVolume();
+    public String getColor();
+}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,19 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author David Truan
+ */
+public class Trumpet implements IInstrument {
+    public String play() {
+        return "pouet";
+    }
+    
+    public int getSoundVolume() {
+        return 10;
+    }
+    
+    public String getColor() {
+        return "golden";
+    }
+}
