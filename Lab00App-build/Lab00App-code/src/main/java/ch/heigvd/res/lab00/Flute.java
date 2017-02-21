b243df11822faba1b9c532236761f2fac3cc5943
Gallouche <gallandat.t@gmail.com>
2017-02-21 20:11:43 +0100

    adding Trumpet.java IInstrument.java Flute.java

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Gallouche
+ */
+public class Flute implements IInstrument {
+
+   @Override
+   public String play() {
+      return null;
+   }
+
+   @Override
+   public String getColor() {
+      return null;
+   }
+
+   @Override
+   public int getSoundVolume() {
+      return 25;
+   }
+   
+}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Gallouche
+ */
+public interface IInstrument {
+
+   String play();
+
+   String getColor();
+
+   int getSoundVolume();
+}
