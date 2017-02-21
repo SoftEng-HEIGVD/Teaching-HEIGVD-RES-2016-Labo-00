b64969687e72c5f979e983d12f1b3cf1f8d799cb
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-02-21 10:30:04 +0000

    Implementation of Trumpet and Flute according to specifications

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Trumpet implements IInstrument{
+    @Override
+    public String play(){
+        return "pouet";
+    }
+    
+    @Override
+    public String getColor(){
+        return "golden";
+    }
+    
+    @Override
+    public int getSoundVolume(){
+        return 100;
+    }
+}
