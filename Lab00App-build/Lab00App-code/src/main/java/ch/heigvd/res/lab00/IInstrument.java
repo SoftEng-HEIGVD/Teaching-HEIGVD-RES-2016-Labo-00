fe9936c4da6249db0203afa43015744c06c9dccb
Lara_Chauffoureaux <lara.chauffoureaux@heig-vd.ch>
2017-02-21 20:30:50 +0100

    Rendu laboratoire 00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,20 @@
+/*
+ * Laboratoire 00
+ * Cours de réseau 2017
+ * HEIG-VD
+ * Created on : 21.02.2017
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Lara Chauffoureaux
+ */
+public interface IInstrument {
+    
+    public String play();
+    
+    public String getColor();
+    
+    public int getSoundVolume();    
+}
