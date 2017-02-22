2c8960383f4a74e694de13c4969c6f272960c656
Mikijonie <mika.pagani@heig-vd.ch>
2017-02-22 12:08:10 +0100

    Implementation of the files according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+public abstract class IInstrument {
+
+	public abstract String play();
+
+	public abstract int getSoundVolume();
+
+	public abstract String getColor();
+
+}
