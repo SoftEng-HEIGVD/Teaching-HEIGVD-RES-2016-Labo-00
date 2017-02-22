0da4c346694ce6b881ab9d2b9bafe1e760ee01e8
qrzeller <qrzeller@users.noreply.github.com>
2017-02-22 21:42:53 +0000

    Adding classes

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,7 @@
+package ch.heigvd.res.lab00;
+
+public interface IInstrument {
+    String play();
+    String getColor();
+    int getSoundVolume();
+}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,8 @@
+package ch.heigvd.res.lab00;
+
+public class Trumpet implements IInstrument{
+    Trumpet(){}
+    public int getSoundVolume(){return 100;}
+    public String play(){return "pouet";}
+    public String getColor(){return "golden";}
+}
