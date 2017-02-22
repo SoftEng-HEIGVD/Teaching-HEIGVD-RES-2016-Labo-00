a36a952c43f0fe41ee9d070c6b883682dd8321d9
Lou <luana.martelli@heig-vd.ch>
2017-02-22 15:28:52 +0100

    Commit labo 00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,13 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by Blue Spring on 22.02.2017.
+ */
+public class Flute extends WindInstrument {
+
+    public Flute() {
+        super("ffff", 5, "black");
+    }
+
+
+}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,10 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by Blue Spring on 22.02.2017.
+ */
+public interface IInstrument {
+    String play();
+    int getSoundVolume();
+    String getColor();
+}
