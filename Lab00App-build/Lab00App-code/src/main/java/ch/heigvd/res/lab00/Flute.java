0cad0a0acee3d05d674f50c22eb7acc7c4318ec9
sandra <annie.dongmongoumnai@heig-vd.ch>
2017-02-23 22:37:21 +0100

    Laboratoire 00

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author annie
+ */
+public class Flute implements IInstrument{
+   public String play(){
+      return "";
+   }
+   public int getSoundVolume(){
+         return 0;
+   }
+   public String getColor(){
+      return "";
+   }
+   
+}
