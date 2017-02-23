86f4aba041a3f96ad72cb7217152371633661c47
GOthGir <gaetan.othenin-girard@heig-vd.ch>
2017-02-23 11:10:53 +0100

    Implementation of IInstrument.java, Trumpet.java and Flute.java according to specitication

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,17 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by gothg on 23.02.2017.
+ */
+public class Flute implements IInstrument {
+   public String play() {
+      return "fuuuuuu";
+   }
+   public int getSoundVolume() {
+      return 10;
+   }
+
+   public String getColor() {
+      return "black";
+   }
+}
