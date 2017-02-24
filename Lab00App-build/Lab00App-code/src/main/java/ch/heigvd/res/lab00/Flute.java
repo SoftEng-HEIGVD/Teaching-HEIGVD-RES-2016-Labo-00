f0a4b4e204d40119324a238a402ada9e590344a0
Tony Clavien <tony.clavien@heig-vd.ch>
2017-02-24 22:38:35 +0100

    Implementation of Instruments and Applications according to the tests

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author Tony Clavien (tony.clavien@heig-vd.ch)
+ */
+public class Flute implements IInstrument{
+
+    @Override
+    public String play() {
+        return "whistle";
+    }
+
+    @Override
+    public int getSoundVolume() {
+        return 1;
+    }
+
+    @Override
+    public String getColor() {
+        return "brown";
+    }
+    
+}
