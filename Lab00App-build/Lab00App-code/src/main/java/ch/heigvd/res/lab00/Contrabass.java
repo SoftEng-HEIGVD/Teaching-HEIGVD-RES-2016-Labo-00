acb60beac9d48a886512f81756dc9b230cbe71d0
Antoine Friant <antoine.friant@gmail.com>
2017-03-01 07:38:33 +0100

    Add implementation for Contrabass

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Contrabass.java
@@ -0,0 +1,12 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Project : Lab00App-build
+ * Author(s) : Antoine Friant
+ * Date : 01.03.17
+ */
+public class Contrabass implements IInstrument {
+    public String play() {
+        return "dzing";
+    }
+}
